132cde573c23c7986fc883eaaa2bb7d7e8360fd8
oliverlaflamme <89362573+oliverlaflamme@users.noreply.github.com>
2021-09-16 22:31:15 -0700

    Add files via upload

---
+++ clock/clock.js
@@ -0,0 +1,97 @@
+/*
+setTimeout(() => {
+    console.log('time!');
+}, 1000);
+
+let i = 0;
+
+function increment() {
+    i++;
+    console.log(i);
+}
+
+let timer = setTimeout(function myTimer() {
+    increment();
+    timer = setTimeout(myTimer, 1000);
+
+}, 1000);
+
+var today = new Date();
+console.log(today);
+
+
+// console.dir(document);
+
+console.log(document.body);
+
+// get element by id 
+
+// console.log(document.getElementById("time-display"));
+
+var displayTime = document.getElementById("time-display");
+console.log(displayTime);
+displayTime.textContent = (1); {
+
+
+*/
+
+function simpleClock() {
+    const date = new Date();
+    const months = ["January", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+    const weekDays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getHours();
+
+
+
+    document.getElementById("time-display").textContent = ` ${months}, ${weekDays}, ${year}`;
+
+
+
+
+    document.getElementById("date-display").textContent = ` ${hours} : ${minutes} : ${seconds}`;
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (hours < 10) {
+        seconds = "0" + seconds;
+    }
+
+
+
+
+}
+
+setInterval(simpleClock, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
